97bb0152dbb5a21a768c3f33b90c30a23ed8db0f
Raffaele Aldrigo <reloader90@gmail.com>
2019-05-01 12:27:09 +0200

    added other folders to MonoContent

--- RpgeOpen.IDE/Utils/MonoContent.cs
+++ RpgeOpen.IDE/Utils/MonoContent.cs
@@ -48,10 +48,26 @@ namespace RpgeOpen.IDE.Utils
                 w.WriteLine("/profile:Reach");
                 w.WriteLine("/compress:False");
 
+                //import audios
+                w.WriteLine("/importer:Mp3Importer");
+                w.WriteLine( "/processor:SongProcessor" );
+                w.WriteLine("/processorParam:Quality=Best");
+
+                var bgmDir = new DirectoryInfo(Path.Combine(project.Directory, Project.Paths.AudioBgm));
+                foreach( var audio in bgmDir.GetFiles()) {
+                    w.WriteLine($"/build:{Project.Paths.AudioBgm}/{audio.Name}");
+                }
+
                 w.WriteLine("/importer:TextureImporter");
                 w.WriteLine("/processor:TextureProcessor");
                 w.WriteLine("/processorParam:ColorKeyColor=255,0,255,255");
 
+                //build Characters
+                var charsDir= new DirectoryInfo( Path.Combine(project.Directory, Project.Paths.Characters) );
+                foreach( var img in charsDir.GetFiles() ) {
+                    w.WriteLine($"/build:{Project.Paths.Characters}/{img.Name}");
+                }
+                //build TileSheets
                 var tilesDir= new DirectoryInfo(Path.Combine(project.Directory, Project.Paths.TileSheets));
                 foreach( var img in tilesDir.GetFiles()) {
                     if(img.Extension.Contains("png") || img.Extension.Contains("jpg") || img.Extension.Contains("jpeg") || img.Extension.Contains("bmp"))
@@ -59,7 +75,7 @@ namespace RpgeOpen.IDE.Utils
                     else
                         w.WriteLine($"/copy:{Project.Paths.TileSheets}/{img.Name}");
                 }
-
+                //copy Maps
                 var mapsDir= new DirectoryInfo(Path.Combine(project.Directory, Project.Paths.Maps));
                 foreach( var map in mapsDir.GetFiles() ) {
                     w.WriteLine($"/copy:{Project.Paths.TileSheets}/{map.Name}");
