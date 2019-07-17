a7288e58cbb1f4cfbc9a241cce9e05987849a132
Raffaele Aldrigo <reloader90@gmail.com>
2019-07-17 16:27:41 +0200

    HashMap

---
+++ RpgeOpen.Models/Exceptions/KeyAlreadyExistException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace RpgeOpen.Models.Exceptions
+{
+    public class KeyAlreadyExistException : Exception
+    {
+        public string Key { get; }
+
+        public KeyAlreadyExistException(string message, string key)
+            :base(message)
+        {
+            Key = key;
+        }
+    }
+}
