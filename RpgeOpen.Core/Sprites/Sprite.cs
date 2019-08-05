b84aa942d2b98503df7cc8c4d61bdb4f9c27a20c
Raffaele Aldrigo <reloader90@gmail.com>
2019-08-05 09:09:38 +0200

    added Sprite

---
+++ RpgeOpen.Core/Sprites/Sprite.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using RpgeOpen.Core.Interfaces;
+using RpgeOpen.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RpgeOpen.Core.Sprites
+{
+    public class Sprite : IGameObject
+    {
+        protected readonly Texture2D spriteSheet;
+        public Size Size { get; private set; }
+        public Point Position { get; set; }
+
+        public Sprite(Texture2D spriteSheet)
+        {
+            this.spriteSheet = spriteSheet;
+            Size = new Size(spriteSheet.Width, spriteSheet.Height);
+        }
+
+        protected Sprite(Texture2D spriteSheet, Size size)
+        {
+            this.spriteSheet = spriteSheet;
+            Size = size;
+        }
+
+        public virtual void Dispose()
+        {
+        }
+
+        public virtual void Draw(GameTime time, SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(spriteSheet, Position.ToVector2(), Color.White);
+        }
+
+        public virtual void Initialize()
+        {
+        }
+
+        public virtual void LoadContent(ContentManager Content)
+        {
+        }
+
+        public virtual void Update(GameTime time)
+        {
+        }
+    }
+}
--- RpgeOpen.Core/Sprites/SpriteCharacter.cs
+++ RpgeOpen.Core/Sprites/SpriteCharacter.cs
@@ -1,15 +1,12 @@
 ﻿using System;
 using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
-using RpgeOpen.Core.Interfaces;
 using RpgeOpen.Models;
 
 namespace RpgeOpen.Core.Sprites
 {
-    public class SpriteCharacter : IGameObject {
+    public class SpriteCharacter : Sprite {
         public Point CornerLeft => new Point(Position.X - Size.Width / 2, Position.Y - Size.Height / 2);
-        public Point Position { get; set; } // => new Point(CornerLeft.X + Size.Width/2, CornerLeft.Y + Size.Height/2);
         public bool IsMoving { get; set; }
         public Direction Direction { get; set; } = Direction.Down;
         /// <summary>
@@ -18,24 +15,12 @@ namespace RpgeOpen.Core.Sprites
         public TimeSpan FrameTime { get; set; } = TimeSpan.FromSeconds(0.33);
         public Rectangle Box => new Rectangle(CornerLeft, new Point(Size.Width, Size.Height));
 
-        /// <summary>
-        /// NumTiles of the sprite
-        /// </summary>
-        public Size Size { get; private set; }
-        private readonly Texture2D spriteSheet;
         private TimeSpan previusFrameTime = TimeSpan.Zero;
         private byte frame = 0;
 
-        public SpriteCharacter( Texture2D spriteSheet, Size size ) {
-            this.spriteSheet = spriteSheet ?? throw new ArgumentNullException(nameof(spriteSheet));
-            this.Size = size;
-        }
+        public SpriteCharacter(Texture2D spriteSheet, Size size) :base(spriteSheet, size) { }
 
-        public void Dispose()
-        {
-        }
-
-        public void Draw(GameTime time, SpriteBatch spriteBatch) {
+        public override void Draw(GameTime time, SpriteBatch spriteBatch) {
             int offset = Size.Height;
             switch( Direction ) {
                 case Direction.Down:
@@ -53,15 +38,7 @@ namespace RpgeOpen.Core.Sprites
             spriteBatch.Draw(spriteSheet, CornerLeft.ToVector2(), new Rectangle(frame * Size.Width, offset, Size.Width, Size.Height), Color.White);
         }
 
-        public void Initialize()
-        {
-        }
-
-        public void LoadContent(ContentManager Content)
-        {
-        }
-
-        public void Update(GameTime time) {
+        public override void Update(GameTime time) {
             if( !IsMoving )
                 return;
 
